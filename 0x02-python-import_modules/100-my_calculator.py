db40ef66508598aaf3204f6f1718ef92b81ab987
SamuelNgundi <samuelwngundi02@gmail.com>
2022-06-02 22:12:26 +0300

    Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
-    import sys
+import sys
+from calculator_1 import add, sub, mul, div
 
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
+if __name__ == "__main__":
+    # check usage
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    op = sys.argv[2]
-    if op != '+' and op != '-' and op != '*' and op != '/':
+    if sys.argv[2] not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    from calculator_1 import add, sub, mul, div
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    a, op, b = sys.argv[1:]
 
+    # choose operation
     if op == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        func = add
     elif op == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        func = sub
     elif op == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        func = mul
     else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        func = div
+
+    # perfom calculation and print result
+    print("{} {} {} = {:d}".format(a, op, b, func(int(a), int(b))))
