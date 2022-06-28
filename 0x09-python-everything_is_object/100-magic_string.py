ec7250391e6d1c77ed6e85d732a9b3043ea7183d
SamuelNgundi <samuelwngundi02@gmail.com>
2022-06-28 21:11:41 +0300

    Magic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.c, t = (magic_string.__dict__.get('c', 0) + 1, ', Holberton')
-    return '{:s}{:s}'.format(t[2:], t * (magic_string.c - 1))
+def magic_string(i=[0]):
+    i[0] += 1
+    return str("Holberton, " * (i[0] - 1)) + "Holberton"
